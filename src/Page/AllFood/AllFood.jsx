7977d1ce433394f6b7c009449dc21303cee6995e
12mhrafi <12mhrafi@gmail.com>
2023-11-08 23:37:39 +0600

    add product displayed

--- src/Page/AllFood/AllFood.jsx
+++ src/Page/AllFood/AllFood.jsx
@@ -47,14 +47,13 @@ const AllFood = () => {
     setCurrentPage(0);
   }
 
-
   const handlePrevious = () => {
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1);
     }
   }
 
-  
+
   const handleNext = () => {
     if (currentPage < pages.length - 1) {
       setCurrentPage(currentPage + 1);
